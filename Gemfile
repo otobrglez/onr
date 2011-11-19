27661fbd6fb69df76373cdceb76266002f4b348f
Oto Brglez <otobrglez@gmail.com>
2011-11-19 11:37:53 +0100

    Database setup added

--- Gemfile
+++ Gemfile
@@ -4,9 +4,16 @@ gem 'sinatra'
 gem 'haml'
 gem 'thin'
 gem 'sass'
+gem 'datamapper'
+
+group :production do
+	gem 'pg'
+	gem 'dm-postgres-adapter'
+end
 
 group :development do
 	gem 'shotgun'
+	gem 'dm-sqlite-adapter'
 end
 
 group :test do
--- onr_app.rb
+++ onr_app.rb
@@ -1,5 +1,22 @@
-
+# By Oto Brglez - <oto.brglez@opalab.com> 
+require 'rubygems'
 require 'sinatra/base'
+require 'data_mapper'
+require 'pathname'
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite://'+Pathname.pwd.join("database.db"))
+
+class Track
+  	include DataMapper::Resource
+	property :id, Serial
+	property :raw, Text
+	property :created_at, DateTime
+end
+
+# DataMapper.auto_migrate!
+DataMapper.finalize
+Track.auto_upgrade!
 
 class OnrApp < Sinatra::Base
 
@@ -10,6 +27,7 @@ class OnrApp < Sinatra::Base
 	end
 
 	get '/' do
+		@tracks = Track.all
 		haml :index
 	end
 
